eb073a4f213620a8717c99b2520a7189fa406c34
Alexander <mralexsaavedra@gmail.com>
2020-11-02 16:19:36 +0100

    01-introducción-a-next-js

---
+++ README.md
@@ -0,0 +1,15 @@
+# Clon de Twitter usando NextJS por @midudev
+
+## Empezar a desarrollar
+
+Puedes levantar el entorno de desarrollo con:
+
+```bash
+npm run dev
+# or
+yarn dev
+```
+
+Abre [http://localhost:3000](http://localhost:3000) y en tu navegador verás el resultado.
+
+**[👨‍🏫 Todas las clases disponibles en la rama principal.](https://github.com/midudev/curso-nextjs-twitter-clone/blob/master/README.md)**
---
+++ pages/index.js
@@ -0,0 +1,47 @@
+import Head from 'next/head'
+import Link from 'next/link'
+
+import AppLayout from '../components/AppLayout'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>devter 👻</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <AppLayout>
+         <h1>
+          <a href="https://nextjs.org">devter</a>
+        </h1>
+        <nav>
+          <Link href="/timeline">
+            <a>
+              timeline
+            </a>
+          </Link>
+        </nav>
+      </AppLayout>
+
+      <style jsx>{`
+        h1 {
+          text-align: center;
+          font-size: 48px;
+        }
+        nav {
+          font-size: 24px;
+          text-align: center;
+        }
+        .another-title {
+          color: #333;
+          font-size: 24px;
+        }
+        a {
+          color: orange;
+          text-decoration: none;
+        }
+      `}</style>
+    </>
+  )
+}
